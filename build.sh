ca8ce3df3a9e2bd2008055f5f28561e77c20b031
Plastikmensch <Plastikmensch@users.noreply.github.com>
2019-09-17 22:43:03 +0200

    exclude tor binaries from app.asar, copy package.json to /src, remove

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 npm update electron
-electron-packager ./src --platform=linux --arch=x64,ia32 --overwrite --asar.unpackDir="tor-linux" --out ./dist
-electron-packager "./src" --platform=win32 --arch=all --overwrite --asar.unpackDir="tor-win32" --icon="./tweetdeck.ico" --out ./dist --win32metadata.CompanyName="Plastikmensch"
+cp package.json ./src/
+electron-packager ./src --platform=linux --arch=x64,ia32 --overwrite --asar --ignore="tor-(linux|win32)" --extra-resource="./src/tor-linux" --out ./dist
+electron-packager "./src" --platform=win32 --arch=all --overwrite --asar --ignore="tor-(linux|win32)" --extra-resource="./src/tor-win32" --icon="./tweetdeck.ico" --out ./dist --win32metadata.CompanyName="Plastikmensch"
+rm ./src/package.json
 echo "Copying neccessary files..."
 cp ./tweetdeck.png ./dist/Tweelectron-linux-ia32
 cp ./tweetdeck.png ./dist/Tweelectron-linux-x64
