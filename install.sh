e5c7baaef9d5ca2bcd8c9214510b8cefb295b3bd
Plastikmensch <Plastikmensch@users.noreply.github.com>
2019-12-25 04:59:21 +0100

    Check if dir exists before removing

--- install.sh
+++ install.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 if [ "$EUID" -ne 0 ]
-  then echo "[Error] Please run as root"
-  exit
+  then
+    echo "[Error] Please run as root"
+    exit
 fi
-rm -r /usr/share/tweelectron
+if [ -d "/usr/share/tweelectron" ]
+  then
+    echo "Old version exists. Removing..."
+    rm -r /usr/share/tweelectron
+fi
+echo "Copying new files"
 cp -r ./dist/Tweelectron-linux-x64/ /usr/share/tweelectron
 echo "Done"
