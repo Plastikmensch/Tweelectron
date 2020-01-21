bdc2f89b101ad44b252ec244c2e2fe51760e1850
Plastikmensch <Plastikmensch@users.noreply.github.com>
2020-01-21 01:25:47 +0100

    turned Settings into object, log function accepts loglevel, handle objects

--- src/common.js
+++ src/common.js
@@ -9,7 +9,7 @@ else {
 }
 
 //const logFile = path.join(app.getPath('userData'), 'tweelectron.log')
-
+/*
 const Settings = [
   [undefined, '"use-tor" :'], //useTor
   [false, '"use-round-pics" :'], //roundPics
@@ -19,8 +19,22 @@ const Settings = [
   [false, '"use-custom-proxy" :'], //useProxy
   ['foopy:80', '"customProxy" :'], //customProxy
   [false, '"links-in-torbrowser" :'], //openInTor
-  ['null', '"tor-browser-exe" :'] //torExe
+  ['null', '"tor-browser-exe" :'], //torExe
+  [0, '"loglevel" :'] //logLevel
 ]
+*/
+const Settings = {
+  useTor: null,
+  useRoundPics: false,
+  theme: 0,
+  width: 1336,
+  height: 720,
+  useCustomProxy: false,
+  customProxy: 'foopy:80',
+  openInTor: false,
+  torBrowserExe: null,
+  logLevel: 0
+}
 
 const settingsFile = SettingsFile()
 function SettingsFile () {
@@ -42,6 +56,50 @@ app.on('ready', () => {
 var methods = {
   readSettings: function () {
     if (fs.existsSync(settingsFile)) {
+      JSON.parse(fs.readFileSync(settingsFile, 'utf8'), function (key, value) {
+        switch (key) {
+          case 'use-tor':
+          case 'useTor':
+            Settings.useTor = value
+            break
+          case 'use-round-pics':
+          case 'useRoundPics':
+            Settings.useRoundPics = value
+            break
+          case 'theme':
+            Settings.theme = value
+            break
+          case 'width':
+            Settings.width = value
+            break
+          case 'height':
+            Settings.height = value
+            break
+          case 'use-custom-proxy':
+          case 'useCustomProxy':
+            Settings.useCustomProxy = value
+            break
+          case 'customProxy':
+            Settings.customProxy = value
+            break
+          case 'links-in-torbrowser':
+          case 'openInTor':
+            Settings.openInTor = value
+            break
+          case 'tor-browser-exe':
+          case 'torBrowserExe':
+            if (value !== 'null') {
+              Settings.torBrowserExe = value
+            }
+            break
+          case 'logLevel':
+            Settings.logLevel = value
+            break
+          default:
+            methods.log(`unknown key found: ${key}`, 0)
+        }
+      })
+      /*
       const settingsData = fs.readFileSync(settingsFile, 'utf8')
       //Redo this mess
       for (let i = 0; i < Settings.length; i++) {
@@ -50,7 +108,7 @@ var methods = {
           Settings[i][0] = settingsData.slice(settingsData.search(Settings[i][1]) + Settings[i][1].length, settingsData.indexOf('\n', settingsData.search(Settings[i][1]))).trim()
         }
         else {
-          this.log('Settings file has wrong format')
+          this.log('Settings file has wrong format', 0)
           const temp = settingsData.split('\n')
           if (i < temp.length - 1) {
             Settings[i][0] = temp[i].slice(temp[i].search('=') + 1)
@@ -72,12 +130,15 @@ var methods = {
         else if (Settings[i][0].search('"') !== -1) {
           Settings[i][0] = Settings[i][0].slice(1, Settings[i][0].lastIndexOf('"'))
         }
-        this.log(`${Settings[i][1]} ${Settings[i][0]}`)
+        this.log(`${Settings[i][1]} ${Settings[i][0]}`, 1)
       }
+      */
     }
     return Settings
   },
   saveSettings: function (Settings) {
+    fs.writeFileSync(settingsFile, JSON.stringify(Settings, null, 4))
+    /*
     let saveSettings = '{\n'
     for (let i = 0; i < Settings.length; i++) {
       if (isNaN(Settings[i][0]) || Settings[i][0] === null) {
@@ -93,11 +154,19 @@ var methods = {
     }
     saveSettings += '}'
     fs.writeFileSync(settingsFile, saveSettings)
-    this.log('Settings saved')
+    */
+    this.log('Settings saved', 0)
   },
-  log: function (message) {
-    fs.appendFileSync(this.logFile, message + '\n')
-    console.log(message)
+  log: function (message, level) {
+    //Settings[9][0] loglevel
+    //add loglevel variable to function
+    if (level <= Settings.logLevel) {
+      if (!Array.isArray(message) && typeof message === 'object') {
+        message = JSON.stringify(message)
+      }
+      fs.appendFileSync(this.logFile, message + '\n')
+      console.log(message)
+    }
   },
   themeDir: path.join(app.getPath('userData'), 'themes'),
   appDir: app.getPath('exe').slice(0, app.getPath('exe').lastIndexOf(path.sep)),
