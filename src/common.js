0c07c8eab4a0ac860fac9231d619f48e866701a5
Plastikmensch <Plastikmensch@users.noreply.github.com>
2020-06-22 18:09:13 +0200

    made readSettings non-accessible, added default loglevel

--- src/common.js
+++ src/common.js
@@ -40,7 +40,64 @@ app.on('ready', () => {
   }
 })
 */
+
+function readSettings () {
+  if (fs.existsSync(settingsFile)) {
+    JSON.parse(fs.readFileSync(settingsFile, 'utf8'), function (key, value) {
+      //Doing Settings[key] = value is more efficient but breaks backwards compability
+      switch (key) {
+        case 'use-tor':
+        case 'useTor':
+          Settings.useTor = value
+          break
+        case 'use-round-pics':
+        case 'useRoundPics':
+          Settings.useRoundPics = value
+          break
+        case 'theme':
+          Settings.theme = value
+          break
+        case 'width':
+          Settings.width = value
+          break
+        case 'height':
+          Settings.height = value
+          break
+        case 'use-custom-proxy':
+        case 'useCustomProxy':
+          Settings.useCustomProxy = value
+          break
+        case 'customProxy':
+          Settings.customProxy = value
+          break
+        case 'links-in-torbrowser':
+        case 'openInTor':
+          Settings.openInTor = value
+          break
+        case 'tor-browser-exe':
+        case 'torBrowserExe':
+          if (value !== 'null') {
+            Settings.torBrowserExe = value
+          }
+          break
+        case 'logLevel':
+          Settings.logLevel = value
+          break
+        default:
+          methods.log(`unknown key found: ${key} with value: ${JSON.stringify(value)}`, 0)
+      }
+    })
+  }
+}
+/*
+function saveSettings () {
+  fs.writeFileSync(settingsFile, JSON.stringify(Settings, null, 4))
+  methods.log('Settings saved', 0)
+  readSettings ()
+}
+*/
 var methods = {
+  /*
   readSettings: function () {
     if (fs.existsSync(settingsFile)) {
       JSON.parse(fs.readFileSync(settingsFile, 'utf8'), function (key, value) {
@@ -90,11 +147,16 @@ var methods = {
     }
     return Settings
   },
+  */
   saveSettings: function (Settings) {
     fs.writeFileSync(settingsFile, JSON.stringify(Settings, null, 4))
     this.log('Settings saved', 0)
+    readSettings()
   },
-  log: function (message, level) {
+  getSettings: function () {
+    return Settings
+  },
+  log: function (message, level = 0) {
     if (level <= Settings.logLevel) {
       if (!Array.isArray(message) && typeof message === 'object') {
         message = JSON.stringify(message)
@@ -107,4 +169,7 @@ var methods = {
   appDir: app.getPath('exe').slice(0, app.getPath('exe').lastIndexOf(path.sep)),
   logFile: path.join(app.getPath('userData'), 'tweelectron.log')
 }
+
+readSettings()
+
 module.exports = methods
