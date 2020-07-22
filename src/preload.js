2e4d0870dd623fb56681174b847f2367c07c25ec
Plastikmensch <Plastikmensch@users.noreply.github.com>
2020-07-22 11:38:29 +0200

    created preload script for child windows

---
+++ src/preload.js
@@ -0,0 +1,25 @@
+const { contextBridge, ipcRenderer } = require('electron')
+const common = require('./common.js')
+// Expose protected methods that allow the renderer process to use
+// the ipcRenderer without exposing the entire object
+contextBridge.exposeInMainWorld(
+  'api', {
+    sendSync: (channel, data) => {
+      // whitelist channels
+      let validChannels = ['Settings', 'Themes']
+
+      if (validChannels.includes(channel)) {
+        return ipcRenderer.sendSync(channel, data)
+      }
+      else {
+        common.log('prevented invalid ipc call', 0)
+      }
+    },
+    common: {
+      log: (message, logLevel = 0) => {
+        common.log(message, logLevel)
+      },
+      Settings: common.Settings
+    }
+  }
+)
